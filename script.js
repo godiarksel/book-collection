b0f636ec6ad4ef8da17bcbc1e10f4cb4daa65b58
godfrey <godiarksel@gmail.com>
2022-06-01 07:04:39 +0300

    Refactor javascript file to use classes

--- script.js
+++ script.js
@@ -1,61 +1,60 @@
 const form = document.querySelector('#form');
 const titleIn = document.querySelector('.title');
 const authorIn = document.querySelector('.author');
-
-// intialize array to get books from localStorage.If no books return an empty string.
 const collection = JSON.parse(localStorage.getItem('Book List')) || [];
-function removeBook(bookTitle, bookAuthor, book) {
-  book.remove();
-  for (let i = 0; i < collection.length; i += 1) {
-    if (collection[i].title === bookTitle && collection[i].author === bookAuthor) {
-      collection.splice(i, 1);
+
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author =  author;
+  }
+  
+   addBook() {
+    const bookList = document.querySelector('.book-list');
+    if (this.title !== '' || this.author !== '') {
+      const book = document.createElement('div');
+      book.innerHTML = `
+      <h2>
+        ${this.title}
+      </h2>
+      <p>
+        ${this.author}
+      </p>
+      `;
+      const rvmBtn = document.createElement('button');
+      rvmBtn.type = 'button';
+      rvmBtn.innerText = 'Remove';
+      book.appendChild(rvmBtn);
+  
+      const divider = document.createElement('hr');
+      book.appendChild(divider);
+      bookList.appendChild(book);
+  
+      rvmBtn.addEventListener('click', () => {
+          book.remove();
+          for (let i = 0; i < collection.length; i += 1) {
+            if (collection[i].title === this.title && collection[i].author === this.author) {
+              collection.splice(i, 1);
+              localStorage.setItem('Book List', JSON.stringify(collection));
+            }
+        }
+      });
+      const myBooks = {};
+      myBooks.title = this.title;
+      myBooks.author = this.author;
+      collection.push(myBooks);
       localStorage.setItem('Book List', JSON.stringify(collection));
+      // localStorage.clear();
+      titleIn.value = '';
+      authorIn.value = '';
     }
   }
 }
 
-// create func to add books to the collection and display the books.
-function addBook() {
-  const bookList = document.querySelector('.book-list');
-  const bookTitle = titleIn.value;
-  const bookAuthor = authorIn.value;
-  if (bookTitle !== '' || bookAuthor !== '') {
-  // create and append all necessary elements of the book list to the div with a book-list class
-    const book = document.createElement('div');
-    book.innerHTML = `
-    <h2>
-      ${bookTitle}
-    </h2>
-    <p>
-      ${bookAuthor}
-    </p>
-    `;
-    const rvmBtn = document.createElement('button');
-    rvmBtn.type = 'button';
-    rvmBtn.innerText = 'Remove';
-    book.appendChild(rvmBtn);
-
-    const divider = document.createElement('hr');
-    book.appendChild(divider);
-    bookList.appendChild(book);
-
-    rvmBtn.addEventListener('click', () => {
-      removeBook(bookTitle, bookAuthor, book);
-    });
-
-    // create object to store book data and then store it in localStorage
-    const books = {};
-    books.title = bookTitle;
-    books.author = bookAuthor;
-    collection.push(books);
-    localStorage.setItem('Book List', JSON.stringify(collection));
-    titleIn.value = '';
-    authorIn.value = '';
-  }
-}
-
-// An eventListener for the Add button
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  addBook();
+  let oneBook = new Book(titleIn.value, authorIn.value);
+  oneBook.addBook();
 });
+
+// console.log(oneBook);
